52980eeafbe2fb69e53652ec9020485a7466c184
thedimas3007 <dima.orischenko@gmail.com>
2024-10-09 18:34:52 +0200

    basic by-url logic

--- logger.py
+++ logger.py
@@ -1,23 +1,33 @@
 from rich.console import Console
 from datetime import datetime
 
+
 class Logger:
     console: Console
-    def __init__(self, console = Console(highlight=False)) -> None:
+
+    def __init__(self, console=Console(highlight=False)) -> None:
         self.console = console
+
     def get_date(self) -> str:
         return datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+
     def debug(self, msg: str) -> None:
         self.console.print(f"[dim bold][{self.get_date()}][/] [cyan bold][-][/] {msg}")
+
     def info(self, msg: str) -> None:
         self.console.print(f"[dim bold][{self.get_date()}][/] [blue bold][*][/] {msg}")
+
     def success(self, msg: str) -> None:
         self.console.print(f"[dim bold][{self.get_date()}][/] [green bold][✔][/] {msg}")
+
     def warn(self, msg: str) -> None:
         self.console.print(f"[dim bold][{self.get_date()}][/] [yellow bold][!][/] {msg}")
+
     def error(self, msg: str) -> None:
         self.console.print(f"[dim bold][{self.get_date()}][/] [red bold][✘][/] {msg}")
+
     def fatal(self, msg: str) -> None:
         self.console.print(f"[dim bold][{self.get_date()}][/] [on red bold][✘][/] {msg}")
-    def input(self, msg = "") -> str:
+
+    def input(self, msg="") -> str:
         return self.console.input(f"[purple bold][→][/] {msg} > ")
--- main.py
+++ main.py
@@ -1,29 +1,23 @@
 import re
 import requests
 import rich
-
-from aiogram import Bot, Dispatcher, executor, types
-from asyncio import get_event_loop
+from requests import HTTPError
 
 from rich.pretty import pprint
+from rich import inspect
+
+from aiogram import Bot, Dispatcher, executor, types
 from yaml import load, dump, Loader
 
 from odesli.Odesli import Odesli
-from youtube_dl import YoutubeDL
+from yt_dlp import YoutubeDL
 from youtubesearchpython import VideosSearch
-from pytube import Search, YouTube, Channel
-from pytube.exceptions import AgeRestrictedError
 from spotipy import Spotify, SpotifyClientCredentials
 
 from os import mkdir, remove
 from os.path import exists
 
 from logger import Logger
-
-# == TODO ==
-# - song.link search and message
-# - Spotify albums support
-
 
 url_regex = r"^(https?:\/\/)?([\da-z\.-]+\.[a-z\.]{2,6})(.*)\/?#?$"
 youtube_domains = ("m.youtube.com", "youtube.com", "www.youtube.com", "youtu.be", "music.youtube.com")
@@ -41,10 +35,11 @@ dp = Dispatcher(bot)
 odesli = Odesli()
 ytdl = YoutubeDL({
     'format': 'bestaudio/best',
+    "outtmpl": "cache/%(id)s.%(ext)s",
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
-        'preferredquality': '192',
+        'preferredquality': '320',
     }],
 })
 spotify = Spotify(auth_manager=SpotifyClientCredentials(
@@ -60,6 +55,62 @@ def save_url(url: str, path: str):
     r = requests.get(url)
     with open(path, "wb") as out:
         out.write(r.content)
+    return path
+
+
+async def handle_song(message: types.Message, song, meta, song_link: str):
+    log.info(f"Downloading {song.id}")
+    await message.edit_text("⏳ Downloading...")
+    ytdl.download(list(song.linksByPlatform.values())[:1])
+
+    log.info(f"Saving thumbnail for {song.id}")
+    thumb = save_url(meta.thumbnailUrl, f"cache/{song.id}.jpg")
+
+    log.info(f"Sending {song.id}")
+    await message.edit_text("⏳ Sending...")
+    await message.answer_audio(types.InputFile(f"cache/{song.id}.mp3"),
+                               caption=f"__[song\\.link]({song_link})__",
+                               parse_mode="MarkdownV2",
+                               performer=meta.artistName,
+                               title=meta.title,
+                               thumb=open(thumb, "rb"))
+    await message.delete()
+    remove(thumb)
+    remove(f"cache/{song.id}.mp3")
+
+
+@dp.message_handler(regexp=url_regex)
+async def handle_url(message: types.Message):
+    # TODO: Add support for playlists (so far only Spotify)
+    new = await message.reply("⏳ Acquiring metadata...")
+    try:
+        log.info(f"Got URL: {message.text} from {message.from_user.full_name} / {message.from_id}")
+        result = odesli.getByUrl(message.text)
+        if "youtube" not in result.songsByProvider.keys():
+            log.warn(f"No YouTube link found for {message.text}")
+            await message.reply("⚠ Song not found!")
+            return
+        yt = result.songsByProvider["youtube"]
+        meta = result.songsByProvider["youtube"]
+        if "spotify" in result.songsByProvider.keys():
+            meta = result.songsByProvider["spotify"]
+        else:
+            log.warn(f"No Spotify link found for {message.text}")
+
+        await handle_song(new, yt, meta, result.songLink)
+    except HTTPError as e:
+        if e.response.status_code >= 400 and e.response.status_code < 500:
+            # TODO: Use YouTube as a fallback for YT links, e.g. podcasts
+            log.warn(f"Code {e.response.status_code} for {message.text}")
+            await new.edit_text("⚠ Song not found!")
+            return
+        else:
+            log.console.print_exception()
+            await new.edit_text("⚠ Unknown HTTP error occurred!")
+    except Exception:
+        log.console.print_exception()
+        await new.edit_text("⚠ Unknown error occurred!")
+        return
 
 
 if __name__ == "__main__":
